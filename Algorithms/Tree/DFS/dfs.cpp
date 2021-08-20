86c9da83da4a035a0c0f26f4574749467c4b7a18
Austin Lamb <alamb0@users.noreply.github.com>
2021-08-19 20:43:34 -0700

    c++ recursive inorder dfs

---
+++ Algorithms/Tree/DFS/dfs.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void dfs(Node *root);
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    dfs(root);
+    return 0;
+}
+
+// inorder dfs
+void dfs(Node *root)
+{
+    if (root != NULL)
+    {
+        dfs(root->left);
+        cout << root->data << endl;
+        dfs(root->right);
+    }
+}
